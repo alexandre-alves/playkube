542fc555d316e371b7e3ed795eab6bef5c9a68a5
José Alexandre <alexandre.alvesm@gmail.com>
2024-06-04 11:43:23 -0300

    Create kubeinit.sh

---
+++ kubeinit.sh
@@ -0,0 +1,3 @@
+kubeadin init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kuberouter.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/application/nginx-app.yaml
